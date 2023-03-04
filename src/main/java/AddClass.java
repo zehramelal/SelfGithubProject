ac377976811f33d4f3fe87d1d8262d5a0d7aaa47
Zehra Melal <melalzehra@gmail.com>
2023-03-04 18:49:39 -0500

    v1

---
+++ src/main/java/AddClass.java
@@ -0,0 +1,13 @@
+public class AddClass {
+
+
+    public static void main(String[] args) {
+
+    addM(3,5);
+
+    }
+
+    public static void addM(int a, int b){
+        System.out.println(a+b);
+    }
+}
