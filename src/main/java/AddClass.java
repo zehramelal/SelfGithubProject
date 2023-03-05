c485b7787923cfb869c76f2bfbf8abab9ad4c862
Zehra Melal <melalzehra@gmail.com>
2023-03-04 19:05:06 -0500

    v2

--- src/main/java/AddClass.java
+++ src/main/java/AddClass.java
@@ -3,12 +3,12 @@ public class AddClass {
 
     public static void main(String[] args) {
 
-    addM(3,5);
+    addM(3,5,4);
 
     }
 
-    public static void addM(int a, int b){
-        System.out.println(a+b);
+    public static void addM(int a, int b, int c){
+        System.out.println(a+b+c);
     }
 }
 
